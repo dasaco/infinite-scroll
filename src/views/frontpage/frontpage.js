16a421c35d82b5fd8a6f673ead240b25bbf8514b
Daniel <danilasanc@gmail.com>
2017-09-11 22:15:50 +0200

    Initial commit

---
+++ src/views/frontpage/frontpage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './frontpage.scss';
+
+class Frontpage extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+	  return (
+			<div>Frontpage</div>
+	  );
+	}
+}
+
+export default Frontpage;
